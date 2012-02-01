ecc5c90783b1eb4faf1df596c4a3d7753d567ff4
Fred Williams <fredwilliams@fullsail.edu>
2012-02-01 00:37:47 -0800

    added files to MIU from VFW

---
+++ js/json.js
@@ -0,0 +1,39 @@
+// Just In Case Application
+// Week: 1
+// Name: Fred Williams
+// Term: MIU - 0212
+// Date: 02/3/2012
+
+
+var json = {
+	"storage1": {
+		"fname": ["First Name:", "Fred"],
+		"foodValue": ["Food:", "Yes"],
+		"suppliesValue": ["Supplies", "Yes"],
+		"adult": ["Adults:", "2"],
+		"child": ["Children:", "3"],
+		"todaysDate": ["Date:", "2012-01-22"],
+		"income": ["Income:", "100"],
+		"comments": ["Comments:", "Test1"]		
+	},
+	"storage2": {
+		"fname": ["First Name:", "Genevieve"],
+		"foodValue": ["Food:", "Yes"],
+		"suppliesValue": ["Supplies", "No"],
+		"adult": ["Adults:", "2"],
+		"child": ["Children:", "3"],
+		"todaysDate": ["Date:", "2012-01-24"],
+		"income": ["Income:", "50"],
+		"comments": ["Comments:", "Test2"]		
+	},
+	"storage3": {
+		"fname": ["First Name:", "Ella"],
+		"foodValue": ["Food:", "Yes"],
+		"suppliesValue": ["Supplies", "No"],
+		"adult": ["Adults:", "2"],
+		"child": ["Children:", "1"],
+		"todaysDate": ["Date:", "2012-01-27"],
+		"income": ["Income:", "10"],
+		"comments": ["Comments:", "Test3"]		
+	}
+}
