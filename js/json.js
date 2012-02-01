8d1872a5cd4645532b72dcad9a91813b511e72c9
Fred Williams <fredwilliams@fullsail.edu>
2012-02-01 01:00:48 -0800

    updated my 20 dummt items.

--- js/json.js
+++ js/json.js
@@ -35,5 +35,193 @@ var json = {
 		"todaysDate": ["Date:", "2012-01-27"],
 		"income": ["Income:", "10"],
 		"comments": ["Comments:", "Test3"]		
+	},
+	"storage4": {
+		"fname": ["First Name:", "Ella"],
+		"foodValue": ["Food:", "Yes"],
+		"suppliesValue": ["Supplies", "No"],
+		"adult": ["Adults:", "2"],
+		"child": ["Children:", "1"],
+		"todaysDate": ["Date:", "2012-01-27"],
+		"income": ["Income:", "10"],
+		"comments": ["Comments:", "Test3"]		
+	},
+	"storage5": {
+		"fname": ["First Name:", "Fred"],
+		"foodValue": ["Food:", "Yes"],
+		"suppliesValue": ["Supplies", "Yes"],
+		"adult": ["Adults:", "2"],
+		"child": ["Children:", "3"],
+		"todaysDate": ["Date:", "2012-01-22"],
+		"income": ["Income:", "100"],
+		"comments": ["Comments:", "Test1"]		
+	},
+	"storage6": {
+		"fname": ["First Name:", "Genevieve"],
+		"foodValue": ["Food:", "Yes"],
+		"suppliesValue": ["Supplies", "No"],
+		"adult": ["Adults:", "2"],
+		"child": ["Children:", "3"],
+		"todaysDate": ["Date:", "2012-01-24"],
+		"income": ["Income:", "50"],
+		"comments": ["Comments:", "Test2"]		
+	},
+	"storage7": {
+		"fname": ["First Name:", "Ella"],
+		"foodValue": ["Food:", "Yes"],
+		"suppliesValue": ["Supplies", "No"],
+		"adult": ["Adults:", "2"],
+		"child": ["Children:", "1"],
+		"todaysDate": ["Date:", "2012-01-27"],
+		"income": ["Income:", "10"],
+		"comments": ["Comments:", "Test3"]		
+	},
+	"storage8": {
+		"fname": ["First Name:", "Ella"],
+		"foodValue": ["Food:", "Yes"],
+		"suppliesValue": ["Supplies", "No"],
+		"adult": ["Adults:", "2"],
+		"child": ["Children:", "1"],
+		"todaysDate": ["Date:", "2012-01-27"],
+		"income": ["Income:", "10"],
+		"comments": ["Comments:", "Test3"]
+	},
+	"storage9": {
+		"fname": ["First Name:", "Ella"],
+		"foodValue": ["Food:", "Yes"],
+		"suppliesValue": ["Supplies", "No"],
+		"adult": ["Adults:", "2"],
+		"child": ["Children:", "1"],
+		"todaysDate": ["Date:", "2012-01-27"],
+		"income": ["Income:", "10"],
+		"comments": ["Comments:", "Test3"]		
+	},
+	"storage10": {
+		"fname": ["First Name:", "Ella"],
+		"foodValue": ["Food:", "Yes"],
+		"suppliesValue": ["Supplies", "No"],
+		"adult": ["Adults:", "2"],
+		"child": ["Children:", "1"],
+		"todaysDate": ["Date:", "2012-01-27"],
+		"income": ["Income:", "10"],
+		"comments": ["Comments:", "Test3"]
+	},	
+	"storage11": {
+		"fname": ["First Name:", "Fred"],
+		"foodValue": ["Food:", "Yes"],
+		"suppliesValue": ["Supplies", "Yes"],
+		"adult": ["Adults:", "2"],
+		"child": ["Children:", "3"],
+		"todaysDate": ["Date:", "2012-01-22"],
+		"income": ["Income:", "100"],
+		"comments": ["Comments:", "Test1"]		
+	},
+	"storage12": {
+		"fname": ["First Name:", "Genevieve"],
+		"foodValue": ["Food:", "Yes"],
+		"suppliesValue": ["Supplies", "No"],
+		"adult": ["Adults:", "2"],
+		"child": ["Children:", "3"],
+		"todaysDate": ["Date:", "2012-01-24"],
+		"income": ["Income:", "50"],
+		"comments": ["Comments:", "Test2"]		
+	},
+	"storage13": {
+		"fname": ["First Name:", "Ella"],
+		"foodValue": ["Food:", "Yes"],
+		"suppliesValue": ["Supplies", "No"],
+		"adult": ["Adults:", "2"],
+		"child": ["Children:", "1"],
+		"todaysDate": ["Date:", "2012-01-27"],
+		"income": ["Income:", "10"],
+		"comments": ["Comments:", "Test3"]		
+	},
+	"storage14": {
+		"fname": ["First Name:", "Ella"],
+		"foodValue": ["Food:", "Yes"],
+		"suppliesValue": ["Supplies", "No"],
+		"adult": ["Adults:", "2"],
+		"child": ["Children:", "1"],
+		"todaysDate": ["Date:", "2012-01-27"],
+		"income": ["Income:", "10"],
+		"comments": ["Comments:", "Test3"]		
+	},
+	"storage15": {
+		"fname": ["First Name:", "Fred"],
+		"foodValue": ["Food:", "Yes"],
+		"suppliesValue": ["Supplies", "Yes"],
+		"adult": ["Adults:", "2"],
+		"child": ["Children:", "3"],
+		"todaysDate": ["Date:", "2012-01-22"],
+		"income": ["Income:", "100"],
+		"comments": ["Comments:", "Test1"]		
+	},
+	"storage16": {
+		"fname": ["First Name:", "Genevieve"],
+		"foodValue": ["Food:", "Yes"],
+		"suppliesValue": ["Supplies", "No"],
+		"adult": ["Adults:", "2"],
+		"child": ["Children:", "3"],
+		"todaysDate": ["Date:", "2012-01-24"],
+		"income": ["Income:", "50"],
+		"comments": ["Comments:", "Test2"]		
+	},
+	"storage17": {
+		"fname": ["First Name:", "Ella"],
+		"foodValue": ["Food:", "Yes"],
+		"suppliesValue": ["Supplies", "No"],
+		"adult": ["Adults:", "2"],
+		"child": ["Children:", "1"],
+		"todaysDate": ["Date:", "2012-01-27"],
+		"income": ["Income:", "10"],
+		"comments": ["Comments:", "Test3"]		
+	},
+	"storage18": {
+		"fname": ["First Name:", "Ella"],
+		"foodValue": ["Food:", "Yes"],
+		"suppliesValue": ["Supplies", "No"],
+		"adult": ["Adults:", "2"],
+		"child": ["Children:", "1"],
+		"todaysDate": ["Date:", "2012-01-27"],
+		"income": ["Income:", "10"],
+		"comments": ["Comments:", "Test3"]
+	},
+	"storage19": {
+		"fname": ["First Name:", "Ella"],
+		"foodValue": ["Food:", "Yes"],
+		"suppliesValue": ["Supplies", "No"],
+		"adult": ["Adults:", "2"],
+		"child": ["Children:", "1"],
+		"todaysDate": ["Date:", "2012-01-27"],
+		"income": ["Income:", "10"],
+		"comments": ["Comments:", "Test3"]		
+	},
+	"storage20": {
+		"fname": ["First Name:", "Ella"],
+		"foodValue": ["Food:", "Yes"],
+		"suppliesValue": ["Supplies", "No"],
+		"adult": ["Adults:", "2"],
+		"child": ["Children:", "1"],
+		"todaysDate": ["Date:", "2012-01-27"],
+		"income": ["Income:", "10"],
+		"comments": ["Comments:", "Test3"]		
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
